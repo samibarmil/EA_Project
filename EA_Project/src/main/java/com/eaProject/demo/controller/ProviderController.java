f0678ac2baa78949a5aebbd4daf98770d09bf1ef
Kaleab Yitbarek Abebe <go4ykal@gmail.com>
2021-05-10 23:04:42 -0500

    refactor ProviderController

--- EA_Project/src/main/java/com/eaProject/demo/controller/ProviderController.java
+++ EA_Project/src/main/java/com/eaProject/demo/controller/ProviderController.java
@@ -1,45 +1,58 @@
 package com.eaProject.demo.controller;
+import com.eaProject.demo.domain.Person;
 import com.eaProject.demo.domain.Session;
+import com.eaProject.demo.services.PersonService;
 import com.eaProject.demo.services.SessionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/session")
+@RequestMapping("/provider")
 public class ProviderController {
 
-
 	@Autowired
-	private SessionService sessionService;
+	PersonService personService;
+	@Autowired
+	SessionService sessionService;
 
-	@GetMapping(path="/provider/sessions",produces = "application/json")
-	List<Session> getAllSession(){
-		return sessionService.getAllSession();
+	@GetMapping(path="/sessions", produces = "application/json")
+	public ResponseEntity<?> getSessions() {
+		Person currentUser = personService.getCurrentUser();
+		List<Session> sessions = sessionService.getSessionsByProvider(currentUser);
+		return ResponseEntity.ok(sessions);
 	}
 
-	@PostMapping(path = "/provider/session/addSession", produces = "application/json")
-	Session addSession(@RequestBody Session session){
-		return sessionService.addSession(session);
+	@PostMapping(path = "/sessions", produces = "application/json")
+	ResponseEntity<?> addSession(@RequestBody Session session){
+		Person currentUser = personService.getCurrentUser();
+		session.setProvider(currentUser);
+		return ResponseEntity.ok(sessionService.addSession(session));
 	}
 
-	//	    // Single Item
-	//	    @GetMapping(path = "/provider/session/{id}", produces = "application/json")
-	//	    Session getSession(@PathVariable long id) throws Exception {
-	//	        return sessionservices.getSession(id)
-	//	                ;
-	//	    }
-
-	@PutMapping(path = "/provider/session/{id}")
-	Session editSession(@RequestBody Session editSession, @PathVariable long id) throws Exception{
-		return sessionService.editSession(editSession, id);
+	@PutMapping(path = "/sessions/{id}")
+	ResponseEntity<?> editSession(@PathVariable Long id, @RequestBody Session session) {
+		try {
+			return ResponseEntity.ok(sessionService.editSession(id, session));
+		} catch (Exception exception) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+		}
 	}
 
-	@DeleteMapping("/provider/session/delete/{id}")
-	void deleteSession(@PathVariable long id) throws Exception{
-		sessionService.deleteSessionById(id);
+	@DeleteMapping("/sessions/{id}")
+	ResponseEntity<?> deleteSession(@PathVariable Long id) {
+		try {
+			Person currentUser = personService.getCurrentUser();
+			sessionService.removeSessionFromProvider(id, currentUser);
+			return ResponseEntity.ok("count : 1");
+		} catch (Exception exception) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+		}
 	}
+
 }
 
 
