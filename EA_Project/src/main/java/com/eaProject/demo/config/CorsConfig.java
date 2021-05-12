f8ed2e4fbcd7037e77e92f94e086319616fed1fd
Kaleab Yitbarek Abebe <go4ykal@gmail.com>
2021-05-12 14:38:27 -0500

    enable CROS configuration to work on all  HTTP methods

--- EA_Project/src/main/java/com/eaProject/demo/config/CorsConfig.java
+++ EA_Project/src/main/java/com/eaProject/demo/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
 				.allowedOrigins("*")
-				.allowedHeaders("*");
+				.allowedHeaders("*").allowedMethods("*");
 			}
 		};
 	}
